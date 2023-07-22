a7c7ec098afc3d63afd9f040c09190706cb1663e
Moises <investmentmoises@gmail.com>
2023-07-22 14:41:07 -0300

    criando estrutura de arra pras rotas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,17 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import CreateUser from "../pages/UserPages/CreateUser/CreateUser";
+import UpdateUser from "../pages/UserPages/UpdateUser/UpdateUser";
 
-const router = createBrowserRouter([
+const userRouter = [
+  {
+    path: "/user/create",
+    element: <CreateUser />,
+  },
+  {
+    path: "/user/update",
+    element: <UpdateUser />,
+  },
+];
+
+const adminRouter = [
   {
     path: "/",
-    element: <div>home</div>,
+    element: <Home />,
   },
+];
 
-  {
-    path: "/users",
-    element: <p>users</p>,
-  },
-  {},
-]);
+//inclui todas as rotas separada por useCase
+const router = createBrowserRouter([...userRouter, ...adminRouter]);
 
 const Routes = () => {
   return <RouterProvider router={router} />;
