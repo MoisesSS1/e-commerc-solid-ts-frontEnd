1175ec77c702e2e21d8acef7f4ac4cb61f759495
Moises <investmentmoises@gmail.com>
2023-08-09 01:07:03 -0300

    correção final, deixando redirecionamento dinamico

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -2,5 +2,5 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoutes = ({ children, redirectTo }: any) => {
   const authenticated = localStorage.getItem("token");
-  return authenticated ? children : <Navigate to={"/"} />;
+  return authenticated ? children : <Navigate to={redirectTo} />;
 };
