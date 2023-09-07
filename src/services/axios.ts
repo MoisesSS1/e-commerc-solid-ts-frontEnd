38215a1d9b3171a714e0de46ffddd4f37c9df468
MoisesSS1 <investmentmoises@gmail.com>
2023-09-07 03:03:21 -0300

    rota backend

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000"
+  baseURL: import.meta.env.BASE_URL || "https://ecomerce-backend1-b05bee7a1a0e.herokuapp.com"
 })
 
 export default api
